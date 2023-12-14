752455f13a20f7b4b00d4953d5e464839d828280
wanjirungugi571 <wanjirungugi571@gmail.com>
2023-12-14 09:22:56 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,25 +9,26 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    /*Variable to store the number of nodes in the list.*/
-    size_t nodes = 0;
+	/*Variable to store the number of nodes in the list.*/
+	size_t nodes = 0;
 
 
-    while (h)
-    {
-        /*Increase the node count.*/
-        nodes++;
+	while (h)
+	{
+		/*Increase the node count.*/
+		nodes++;
 
 
-        /*Print the value of the current node.*/
-        printf("%d\n", h->n);
+		/*Print the value of the current node.*/
+		printf("%d\n", h->n);
 
 
-        /*Move to the next node.*/
-        h = h->next;
-    }
+		/*Move to the next node.*/
+		h = h->next;
+	}
 
 
-    /*Return the total number of nodes in the dlistint_t list.*/
-    return (nodes);
+
+	/*Return the total number of nodes in the dlistint_t list.*/
+	return (nodes);
 }
