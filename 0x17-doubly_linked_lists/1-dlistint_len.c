2a210b12485f771428fabea75b031670341546b8
wanjirungugi571 <wanjirungugi571@gmail.com>
2023-12-14 09:41:54 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 
 
 /**
- * dlistint_len - Func counts the number of elements in a linked dlistint_t list.
+ * dlistint_len - counts the number of elements in a linked dlistint_t list.
  * @h: The head of the dlistint_t list.
  *
  * Return: The number of elements in the dlistint_t list.
@@ -10,22 +10,22 @@
 size_t dlistint_len(const dlistint_t *h)
 {
     /*Variable to store the number of elements in the dlistint_t list.*/
-    size_t nodes = 0;
+	size_t nodes = 0;
 
 
-    while (h)
-    {
-        /*Increase the element count.*/
-        nodes++;
+	while (h)
+	{
+		/*Increase the element count.*/
+		nodes++;
 
 
 
 
-        /*Move to the next element.*/
-        h = h->next;
-    }
+		/*Move to the next element.*/
+		h = h->next;
+	}
 
 
-    /*Return the total number of elements in the dlistint_t list.*/
-    return (nodes);
+	/*Return the total number of elements in the dlistint_t list.*/
+	return (nodes);
 }
